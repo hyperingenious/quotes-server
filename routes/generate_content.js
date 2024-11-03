698944c4c1985601c0811812050a286dffbce8e6
hyperingenious <skbmasale941@gmail.com>
2024-11-03 07:04:30 +0530

    FIX: fixed generate_contnet

--- routes/generate_content.js
+++ routes/generate_content.js
@@ -1,27 +1,28 @@
 const {
   get_all_chunk_ids_with_book_id,
   get_chunk_by_id,
-  add_blogs_and_quotes,
+  add_blogs,
 } = require("../appwrite/appwrite");
-const { ai_blog_quote_generator } = require("../ai/ai_blog_generator");
+const { ai_blog_generator } = require("../ai/ai_blog_generator");
 const { random_chunk } = require("../parser/chunk_random");
 const crypto = require("crypto");
 const fs = require("fs").promises;
 
 async function generateContent(req, res) {
-  const bookId = req.query.id;
-  if (!bookId) {
+  const { id: book_id, user_id } = req.query;
+
+  if (!book_id) {
     return res.status(400).json({ message: "Book ID is required" });
   }
 
-  console.log(`Generating content for book with ID: ${bookId}`);
+  console.log(`Generating content for book with ID: ${book_id}`);
 
   let random_file_name; // Declare the file name variable outside of try-catch
   try {
-    const chunkIds = await get_all_chunk_ids_with_book_id(bookId);
+    const chunkIds = await get_all_chunk_ids_with_book_id(book_id);
 
     if (chunkIds.length === 0) {
-      console.log(`No chunks found for book with ID: ${bookId}`);
+      console.log(`No chunks found for book with ID: ${book_id}`);
       return res.status(404).json({ message: "No chunks found for this book" });
     }
 
@@ -44,14 +45,14 @@ async function generateContent(req, res) {
     await fs.writeFile(random_file_name, random_20_percent_chunk_text);
 
     const random_cache_model_name = `${crypto.randomUUID()}`;
-    const blog_and_quote_chunks = await ai_blog_quote_generator(
+    const blog_and_quote_chunks = await ai_blog_generator(
       random_file_name,
       random_cache_model_name
     );
 
-    await add_blogs_and_quotes(blog_and_quote_chunks, bookId);
+    await add_blogs(blog_and_quote_chunks, book_id, user_id);
 
-    console.log(`Content generated successfully for book with ID: ${bookId}`);
+    console.log(`Content generated successfully for book with ID: ${book_id}`);
     return res.status(200).json({ message: "Content generated successfully" });
   } catch (error) {
     console.error(`Error generating content: ${error.message}`);
