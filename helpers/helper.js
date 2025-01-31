0be69beb46a51dc0a4d8509f42ab1c5ccc0b68ed
hyperingenious <skbmasale941@gmail.com>
2025-01-31 21:39:25 +0530

    TEST MINOR

--- helpers/helper.js
+++ helpers/helper.js
@@ -30,7 +30,24 @@ async function invalidateToken({ req, res }) {
         if (!verifiedToken) {
             return res.status(401).json({ error: 'Unauthorized' });
         }
-        console.log(verifiedToken)
+        /*
+        TOKEN FORMAT 
+        {
+             app_metadata: {},
+             aud: 'authenticated',
+              azp: 'https://purplenight.hyperingenious.tech',
+              email: 'skbmasale941@gmail.com',
+              exp: 1738339522,
+              iat: 1738339462,
+              iss: 'https://assured-ape-25.clerk.accounts.dev',
+              jti: '09765b21198ba09b3a56',
+              nbf: 1738339457,
+              role: 'authenticated',
+              sub: 'user_2oFLUNePrbPyBH1zJL4gV4mn7Kp',
+              user_metadata: {}
+              }
+        */
+        console.log(verifiedToken.user_metadata)
         return verifiedToken;
 
     }
