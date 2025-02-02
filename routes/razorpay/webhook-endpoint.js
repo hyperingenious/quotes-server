1104fbb95ff8981ced532cccb8c865960da6db63
hyperingenious <skbmasale941@gmail.com>
2025-02-02 16:34:17 +0530

    ADD: cancels subscription entriy if for duplicate payment_id

--- routes/razorpay/webhook-endpoint.js
+++ routes/razorpay/webhook-endpoint.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const { validateWebhookSignature } = require('razorpay/dist/utils/razorpay-utils')
 const { INITIATED_TRANSACTIONS_COLLECTION_ID, DATABASE_ID, databases } = require("../../appwrite/appwrite");
+const { validateWebhookSignature } = require('razorpay/dist/utils/razorpay-utils');
+const { INITIATED_TRANSACTIONS_COLLECTION_ID, DATABASE_ID, databases, SUBSCRIPTIONS_COLLECTION_ID } = require("../../appwrite/appwrite");
 const { add_subscription_quota, add_subscriptions_entry } = require("../../appwrite/add/add_appwrite");
+const sdk = require("node-appwrite");
 
 async function razorpayWebhookEndpoint(req, res) {
     try {
@@ -38,12 +41,19 @@ async function razorpayWebhookEndpoint(req, res) {
         end_date.setDate(start_date.getDate() + 30);
 
         const added_document = await add_subscriptions_entry({ payment_id: paymentEntity.id, user_id: document.user_id, subscription_type: document.subscription_type, start_date: start_date.toISOString(), end_date: end_date.toISOString(), payment_method: paymentEntity.method, amount: paymentEntity.amount, currency: paymentEntity.currency });
+        // Check for existing subscription using the unique payment ID to prevent duplicates.
+        const { total, documents: existingSubscriptions } = await databases.listDocuments(DATABASE_ID, SUBSCRIPTIONS_COLLECTION_ID, [sdk.Query.equal('payment_id', paymentEntity.id)]);
+        if (total > 0 || existingSubscriptions.length > 0) {
+            console.log("Existing subscription found for payment ID:", paymentEntity.id);
+            return; // Return 200 OK if subscription already exists.
+        }
         console.log("Subscription entry added successfully.");
 
         await add_subscription_quota({ subscription_id: added_document.$id })
         console.log("Subscription quota entry added successfully");
 
-        return;  // Return success response
+        // Return success response
+        return;
     } catch (error) {
         console.error("Error processing Razorpay webhook:", error);
     }
