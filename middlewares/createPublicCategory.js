9587be00ed94c43c3db842c09951cadca0f1e275
hyperingenious <skbmasale941@gmail.com>
2025-03-19 02:25:50 +0530

    FIX: Create public category has been fixed

--- middlewares/createPublicCategory.js
+++ middlewares/createPublicCategory.js
@@ -7,7 +7,7 @@ async function createPublicCategory(req, res, next) {
         const { total } = await databases.listDocuments(
             DATABASE_ID,
             CATEGORY_COLLECTION_ID,
-            [Query.equal('user_id', verifiedToken.sub), Query.equal('category', 'public')]
+            [Query.equal('user_id', verifiedToken.sub), Query.equal('category_name', 'public')]
         )
 
         if (total === 0) {
@@ -16,7 +16,7 @@ async function createPublicCategory(req, res, next) {
                 CATEGORY_COLLECTION_ID, ID.unique(),
                 {
                     user_id: verifiedToken.sub,
-                    category: 'public'
+                    category_name: 'public'
                 }
             )
         }
